da760f44b776c120149c1093c1a65ce2dccc6316
pedro23542 <arsensankin@yandex.ru>
2020-11-26 15:56:31 +0500

    added product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
-$(function(){                 // сначала будет обрабатыватся только то что в index а затем js
+$(function(){     
+  
+  
+  // сначала будет обрабатыватся только то что в index а затем js
+  $(".rate-star").starRating({
+    initialRating: 4,
+    strokeWidth: 0,
+    starSize: 25,
+    useGradient: false,
+    starSize: 12,
+    readOnly: true,
+  });
 
+  $('.product-sdlider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,8 @@ gulp.task('css', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/star-rating-svg/src/css/star-rating-svg.css',
+
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -47,6 +49,8 @@ gulp.task('js', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
+   'node_modules/star-rating-svg/src/jquery.star-rating-svg.js',
+
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
