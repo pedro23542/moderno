aa0c12685678b0b9b1e89233a48137bf43b00118
pedro23542 <arsensankin@yandex.ru>
2021-02-12 23:53:10 +0500

    added withdrawals

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function(){     
   
+  var mixer = mixitup('.products__inner-box');
+//   var mixer = mixitup('.products__inner-box', {
+//     selectors: {
+//         target: '.product__item'
+//     },
+//     animation: {
+//         duration: 300
+//     }
+// });
+
   // сначала будет обрабатыватся только то что в index а затем js
   $(".rate-star").starRating({
     initialRating: 4,
@@ -14,7 +24,32 @@ $(function(){
     dots: true,
     arrows: false,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   });
 
   $('.menu__btn').on('click', function(){
@@ -24,16 +59,18 @@ $(function(){
   $('.header__btn-menu').on('click', function(){
     $('.header__box').toggleClass('active');
   });
+
+  $('input[type="file"], select').styler();
+
+  //tabs
+  $('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
+    let id = $(this).attr('data-id');
+    $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+    $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
+    $(this).addClass('active');
+    $('#' + id).addClass('active-tab').fadeIn();
+    return false;
+  });
+
 });
 
-$('input[type="file"], select').styler();
-
-//tabs
-$('.product-one__tabs .tab, settings__tabs .tab').on('click', function(event) {
-  let id = $(this).attr('data-id');
-  $('.product-one__tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-  $('.product-one__tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
-  $(this).addClass('active');
-  $('#' + id).addClass('active-tab').fadeIn();
-  return false;
-});
